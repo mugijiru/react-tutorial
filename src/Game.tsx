3f6d95d6727aa99f2090d45969dfeabe99dfb9fb
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-09-11 12:47:58 +0900

    自動補正の適用

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,58 +6,58 @@ type History = {
   squares: TSquare[]
 }
 
-const Game = (): JSX.Element => {
-  const [history, setHistory] = useState<History[]>([{ squares: Array(9).fill(null) }])
-  const [stepNumber, setStepNumber] = useState<number>(0)
-  const [xIsNext, setXIsNext] = useState<boolean>(true)
+function Game(): JSX.Element {
+    const [history, setHistory] = useState<History[]>([{ squares: Array(9).fill(null) }])
+    const [stepNumber, setStepNumber] = useState<number>(0)
+    const [xIsNext, setXIsNext] = useState<boolean>(true)
 
-  const handleClick = (i: number) => {
-    const _history = history.slice(0, stepNumber + 1)
-    const current = _history[_history.length - 1]
-    const squares = current.squares.slice()
-    if (calculateWinner(squares) || squares[i]) { return }
+    const handleClick = (i: number) => {
+        const _history = history.slice(0, stepNumber + 1)
+        const current = _history[_history.length - 1]
+        const squares = current.squares.slice()
+        if (calculateWinner(squares) || squares[i]) { return}
 
-    squares[i] = xIsNext ? 'X' : 'O'
+        squares[i] = xIsNext ? 'X' : 'O'
 
-    setHistory(_history.concat([{ squares }]))
-    setStepNumber(_history.length)
-    setXIsNext(!xIsNext)
-  }
+        setHistory(_history.concat([{ squares }]))
+        setStepNumber(_history.length)
+        setXIsNext(!xIsNext)
+    }
 
-  const jumpTo = (step: number) => {
-    setStepNumber(step)
-    setXIsNext((step % 2) === 0)
-  }
+    const jumpTo = (step: number) => {
+        setStepNumber(step)
+        setXIsNext((step % 2) === 0)
+    }
 
-  const current = history[stepNumber]
-  const winner = calculateWinner(current.squares)
+    const current = history[stepNumber]
+    const winner = calculateWinner(current.squares)
 
-  const moves = history.map((_step, move) => {
-    const desc = move ? `Go to move #${move}` : 'Go to game start'
+    const moves = history.map((_step, move) => {
+        const desc = move ? `Go to move #${move}` : 'Go to game start'
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        )
+    })
+    let status
+    if (winner) {
+        status = `Winner: ${winner}`
+    } else {
+        status = `Next player: ${xIsNext ? 'X' : 'O'}`
+    }
+
     return (
-        <li key={move}>
-          <button onClick={() => jumpTo(move)}>{desc}</button>
-        </li>
+        <div className="game">
+            <div className="game-board">
+                <Board squares={current.squares} onClick={(i: number) => handleClick(i)} />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
     )
-  })
-  let status
-  if (winner) {
-    status = `Winner: ${winner}`
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board squares={current.squares} onClick={(i: number) => handleClick(i)} />
-      </div>
-      <div className="game-info">
-        <div>{status}</div>
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
 }
 
 function calculateWinner(squares: TSquare[]): string | null {
