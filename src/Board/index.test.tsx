819ebc53180b1b9483d9b74782e00c4ccb6b1399
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-09-16 01:31:36 +0900

    与えられた文字が表示されるということを確認するテストを追加

--- src/Board/index.test.tsx
+++ src/Board/index.test.tsx
@@ -2,11 +2,24 @@ import { render, screen } from '@testing-library/react'
 
 import Board from '.'
 
-jest.mock('../Square', () => () => (<div data-testid="mocked-square" ></div>))
+jest.mock('../Square', () => ({ value }: { value: string | null }) => {
+  return (
+    <button type="button" data-testid="mocked-square">
+      {value}
+    </button>
+  )
+})
 
 describe('Board', () => {
   it('Square が9つ表示されること', () => {
-    render(<Board squares={Array(9).fill(null)} onClick={() => { }} />)
+    render(<Board squares={Array(9).fill(null)} onClick={() => {}} />)
     expect(screen.queryAllByTestId("mocked-square").length).toEqual(9)
+  })
+
+  it('各 Square には squares で与えられた文字が表示されること', () => {
+    const squares = ["X", "X", "O", null, null, null, null, null, null]
+    render(<Board squares={squares} onClick={() => { }} />)
+    expect(screen.queryAllByText("X").length).toEqual(2)
+    expect(screen.queryAllByText("O").length).toEqual(1)
   })
 })
