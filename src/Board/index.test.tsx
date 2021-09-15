8bfd9cc586a3dfc29be2febbeb7ccf61dd051225
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-09-16 01:44:12 +0900

    Click 関数が子コンポーネントに渡ることを確認するテストを追加
    テストの中で実質 Square コンポーネントを実装しているので
    正直やりすぎな気はしているが
    とりあえず練習ということで勘弁して

--- src/Board/index.test.tsx
+++ src/Board/index.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import Board from '.'
 
-jest.mock('../Square', () => ({ value }: { value: string | null }) => {
+jest.mock('../Square', () => ({ value, onClick }: { value: string | null, onClick: () => void }) => {
   return (
-    <button type="button" data-testid="mocked-square">
+    <button type="button" data-testid="mocked-square" onClick={onClick}>
       {value}
     </button>
   )
@@ -21,5 +21,16 @@ describe('Board', () => {
     render(<Board squares={squares} onClick={() => { }} />)
     expect(screen.queryAllByText("X").length).toEqual(2)
     expect(screen.queryAllByText("O").length).toEqual(1)
+  })
+
+  it('各 Square が Click された時は渡した関数が実行されること', () => {
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation()
+    render(<Board squares={Array(9).fill(null)} onClick={() => { alert("clicked!") }} />)
+
+    screen.getAllByTestId("mocked-square").forEach((square) => {
+      fireEvent.click(square)
+      expect(window.alert).toBeCalledWith('clicked!')
+    })
+    expect(alertMock).toHaveBeenCalledTimes(9)
   })
 })
